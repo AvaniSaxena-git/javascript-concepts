41ce5135b733e8856646f9ae2130f8ba362b89b5
Avani saxena <avanisaxena2005@gmail.com>
2024-12-22 08:44:57 +0000

    compaisons and sonversion done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -10,3 +10,18 @@ console.log(valueInNum)
 let someNum = 33;
 let stringNum = String(someNum)
 console.log(typeof stringNum)
+
+//---------------------------------------Operations-------------------------------------------
+
+let value = 5;
+let negVAlue = -value;
+console.log(negVAlue)
+
+let str1 = "hello"
+let sttr2 = " sir"
+let str3 = str1 + str2 
+console.log(str3)
+console.log("1" +2)
+console.log(2 + "5")
+console.log("1" +5 +6)
+console.log(5+1+"5") // only this will add starting two elements
---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,28 @@
+console.log(2>7)
+console.log(4==4)
+console.log(2<9)
+
+
+//------------------//
+
+
+console.log("4"<8)
+console.log(4>"8")
+
+//----------------------//
+
+
+console.log(null>0)
+console.log(null==0)
+console.log(null>=0)
+ 
+
+//---------------//
+
+console.log(undefined==0)
+
+// "==" checks value
+// "===" checks data type
+
+console.log("2"==2)
+console.log("2"===2)
